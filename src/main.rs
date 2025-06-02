8a6bc44b670dfc8a56e3ef6d44a57e0f342fabba
Kaamaru <peaparanut@gmail.com>
2025-06-03 02:27:57 +0700

    web

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use dioxus::prelude::*;
-
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const INTER_FONT: &str = "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap";
+const PRECONNECT_GOOGLE_FONTS: &str = "https://fonts.googleapis.com";
+const PRECONNECT_GSTATIC: &str = "https://fonts.gstatic.com";
 
 fn main() {
     dioxus::launch(App);
@@ -11,27 +12,95 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        // Meta & Stylesheets
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
+        document::Link { rel: "preconnect", href: PRECONNECT_GOOGLE_FONTS }
+        document::Link {
+            rel: "preconnect",
+            href: PRECONNECT_GSTATIC,
+            crossorigin: "anonymous",
+        }
+        document::Link { rel: "stylesheet", href: INTER_FONT }
 
-    }
-}
+        // Page Layout
+        div { class: "container",
+            header { class: "header",
+                h1 { "Hi, me Kaamaru" }
+                p {
+                    "Lazy ahh rust "
+                    s { "developer" }
+                    " who chose the "
+                    i { "wrong?" }
+                    " path instead of the popular JS and can't do shi"
+                }
+            }
+            main { class: "main-content",
+                section {
+                    h2 { "About Me" }
+                    ul {
+                        li { "I make games, bad and unfinished ones." }
+                        li { "I can't make webapps." }
+                    }
+                }
+                section {
+                    h2 { "Projects" }
+                    ul {
+                        li { "Disappointment and harm to some people." }
+                        li {
+                            "Rusty Heavy Seabot game "
+                            span { style: "color: gray; font-size: 0.9em;", "(currently private)" }
+                        }
+                    }
+                }
+                section {
+                    h2 { "Contact" }
 
-#[component]
-pub fn Hero() -> Element {
-    rsx! {
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+                    p {
+                        // Discord
+                        img {
+                            src: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/svg/1f4ac.svg", // speech balloon icon
+                            alt: "Discord",
+                            style: "width: 1em; height: 1em; vertical-align: middle; margin-right: 0.4em;",
+                        }
+                        "Me on Discord: "
+                        u {
+                            b { "kaamaru" }
+                        }
+                        br {}
+
+                        span { style: "color: gray; font-size: 0.9em;",
+                            "this is literally from discord..."
+                        }
+                        br {}
+                        br {}
+
+                        // GitHub
+                        img {
+                            src: "https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/github.svg",
+                            alt: "GitHub",
+                            style: "width: 1em; height: 1em; vertical-align: middle; margin-right: 0.4em; filter: invert(100%);",
+                        }
+                        a {
+                            href: "https://github.com/Kaamaru",
+                            target: "_blank",
+                            "Kaamaru on GitHub"
+                        }
+                    }
+
+                    iframe {
+                        title: "Discord user embed",
+                        width: "340",
+                        height: "192",
+                        frame_border: "0",
+                        src: "https://widgets.vendicated.dev/user?id=1143217634263965777&theme=dark&banner=true&full-banner=true&rounded-corners=true&discord-icon=true&badges=true&guess-nitro=false&",
+                    }
+                }
+            }
+            footer { class: "footer",
+                p { "© 2025 Kaamaru. Built with Rust and Dioxus." }
             }
         }
     }
 }
+
